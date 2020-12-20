197eeaf8c58ef5b46f086a511efe89eff43ebd0e
MARIKO ALVARADO <marikoaalvarado@gmail.com>
2020-12-19 21:15:06 -0800

    challenge10

---
+++ javascript/code-challenges/stacksAndQueues/stack.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const Node = require('./node');
+
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+
+  push(value) {
+    // O(1)
+    let node = new Node(value);
+    node.next = this.top;
+    this.top = node;
+  }
+
+  pop() {
+    // O(1)
+    // if (!this.top) { return this.top; }
+    if (!this.top) { throw new Error('Stack is empty'); }
+
+    const item = this.top.value;
+    this.top = this.top.next;
+    return item;
+  }
+
+  peek() {
+    // O(1)
+    if (!this.top) { throw new Error('Stack is empty'); }
+    return this.top.value;
+  }
+
+  isEmpty() {
+    return (!this.top) ? true : false;
+  }
+}
+module.exports = Stack;
