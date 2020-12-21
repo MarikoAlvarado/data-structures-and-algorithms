93cbcc3ecfbaed0cbbe85fdc453029032ceb177c
MARIKO ALVARADO <marikoaalvarado@gmail.com>
2020-12-21 14:56:33 -0800

    challenge11

---
+++ javascript/code-challenges/queueWithStacks/queStacks.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let PseudoQueue = require('./queue-with-stacks');
+//test 1
+describe('enqueueing', () => {
+  it('should push multiple people into stack A', () => {
+    const queue = new PseudoQueue();
+    queue.enqueue('simon')
+    queue.enqueue('nathan')
+    queue.enqueue('ricardo')
+    console.log(this.stackA);
+    expect(queue.stackA[0]).toEqual('simon');
+  })
+})
+
+describe('dequeueing', () => {
+  it('should push value from stackA into stackB', () => {
+    const queue = new PseudoQueue();
+    queue.enqueue('simon')
+    queue.enqueue('nathan')
+    queue.enqueue('ricardo')
+
+    queue.dequeue();
+    expect(queue.stackB[0]).toEqual('ricardo');
+  })
+})
